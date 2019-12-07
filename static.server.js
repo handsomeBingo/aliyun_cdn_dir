c0ad1d4794bc3a0e6f3dc030bc64b0c5aeef6df6
handsomeBingo <1164664451@qq.com>
2019-12-07 18:22:25 +0800

    rm middle-ware

--- static.server.js
+++ static.server.js
@@ -3,9 +3,6 @@ const path = require('path');
 
 let app = express();
 
-app.use(express.static(__dirname))
-app.use(function (req, res) {
-  console.log(req.url)
-})
+app.use(express.static(__dirname));
 
 app.listen(8081, () => console.log('the server cdn is running on 8081'))
